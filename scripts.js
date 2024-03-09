90e49aa467c7b5b91d8e5834c51ad51a110dfefa
Giovana1309 <giovanaccolivera@gmail.com>
2024-03-09 01:05:47 -0300

    alter

--- scripts.js
+++ scripts.js
@@ -1,11 +1,35 @@
 const convertButton=document.querySelector(".convert-button")
+const currencySelect =document.querySelector(".currency-select")
 
 function convertValues (){
 
     const inputCurrencyValue=document.querySelector (".input-currency").value
-    console.log(inputCurrencyValue)
+    const currencyValueToConvert= document.querySelector(".currency-value-to-convert")
+    const currencyValueToConverted = document.querySelector(".currency-value")
+    console.log (currencySelect)
     const dolarToday= 5.2
+    const eurotoday=6.2
+
     const convertValue = inputCurrencyValue/dolarToday
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat 
+    ("pt-br",{
+style:"currency",
+currency: "BRL"
+
+    }) .format(inputCurrencyValue)
+
+
+    currencyValueToConverted.innerHTML = new Intl.NumberFormat ("en-US",{
+
+style:"currency",
+currency: "USD"
+
+    }) .format (convertValue)
+    
+
+
+
     console.log (convertValue)
 
 } 
